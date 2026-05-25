632ff791d5b3a0d3a7fcc2bd97d8efa99f6de853
thibault <thibault.huet.bzh@gmail.com>
2026-05-25 20:29:25 +0200

    correctif css

--- admin.php
+++ admin.php
@@ -37,6 +37,7 @@ closeDB($conn);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel Admin - Bwat Let</title>
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
     <?php include("static/header.php"); ?>
