d0fdc316c14916266cf5f6d9c30f8751ac61adfb
thibault <thibault.huet.bzh@gmail.com>
2026-05-25 21:24:25 +0200

    logo

--- creer_film.php
+++ creer_film.php
@@ -85,6 +85,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajouter un film - Bwat Let</title>
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
     <?php include("static/header.php"); ?>
